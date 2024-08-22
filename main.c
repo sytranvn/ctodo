940aab282616074c0f5adb2940b1d045133021c0
Sy Tran Dung <sytrancsvn@gmail.com>
2024-08-22 13:46:50 +0700

    add td

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <time.h>
 
+void repl(sqlite3 *db, user_t *user);
+
 int main() {
   sqlite3 *db;
   sqlite3_open("todo.db", &db);
@@ -15,7 +17,7 @@ int main() {
   char *username = getenv("USER");
   if ((rc = get_user(db, username, &user)) == SQLITE_DONE && user != NULL) {
 
-    printf("Hi, %s since %s\n", user->name, asctime(&user->create_tm));
+    printf("Hi, %s\n", user->name);
   } else {
     user = malloc(sizeof(*user));
 
@@ -24,7 +26,7 @@ int main() {
     if ((rc = add_user(db, user)) != SQLITE_OK) {
       fprintf(stderr, "Add user failed with error code %d\n", rc);
     } else {
-      printf("Created user %s, id: %ld, create time: %s\n", user->name,
+      printf("Created user %s, id: %lld, create time: %s\n", user->name,
              user->id, asctime(&user->create_tm));
     }
   }
@@ -37,7 +39,7 @@ int main() {
     if (tasks != NULL && task_cnt) {
       printf("You have %ld tasks:\n", task_cnt);
       for (int i = 0; i < task_cnt; i++) {
-        printf("id: %ld\n"
+        printf("id: %lld\n"
                "description: %s\n"
                "completed: %s\n",
                tasks[i].id, tasks[i].description,
@@ -55,24 +57,45 @@ int main() {
       printf("You have no tasks.\n");
   } else
     printf("Error %d\n", rc);
-  printf("Add tasks: ");
-  char *str = malloc(256);
-  size_t len;
-  getline(&str, &len, stdin);
-  str[strlen(str) - 1] = '\0';
-  task_t *todo = malloc(sizeof(*todo));
-  todo->description = str;
-  if ((rc = add_task(db, user, todo)) == SQLITE_OK) {
-    printf("Added task\n");
-  } else {
-    printf("Error %d\n", rc);
+
+  while (1) {
+    repl(db, user);
   }
 
   sqlite3_close(db);
-  free(todo);
   free(user);
   if (tasks != NULL) {
     free(tasks);
   }
   return 0;
+}
+
+void repl(sqlite3 *db, user_t *user) {
+  int rc;
+  printf("> ");
+  char *cmd_line;
+  char *cmd;
+  size_t lc = 0;
+  getline(&cmd_line, &lc, stdin);
+  printf("%s\n", cmd_line);
+
+  sscanf(cmd_line, "%s", cmd);
+
+  printf("%s\n", cmd);
+  if (strcmp(cmd, "a") == 0) {
+    printf("Add tasks: ");
+    char *str;
+    size_t len = 0;
+    getline(&str, &len, stdin);
+    str[strlen(str) - 1] = '\0';
+    task_t *todo = malloc(sizeof(*todo));
+    todo->description = str;
+    if ((rc = add_task(db, user, todo)) == SQLITE_OK) {
+      printf("Added task\n");
+    } else {
+      printf("Error %d\n", rc);
+    }
+  } else {
+    printf("Invalid command, h for help.\n");
+  }
 }
